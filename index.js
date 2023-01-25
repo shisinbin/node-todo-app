e5df44f905f1f426509b13e24238b33e871be911
ssb <sb1664@gmail.com>
2023-01-25 21:41:31 +0000

    complete standard version

---
+++ index.js
@@ -0,0 +1,9 @@
+const showMainMenu = require('./lib/showMainMenu');
+
+function init() {
+  console.log('Welcome to the Todo Pro App!\n----------------------------');
+
+  showMainMenu();
+}
+
+init();
